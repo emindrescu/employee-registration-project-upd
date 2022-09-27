cc826ff9603edde77cf5b46b118c8addb79c2680
CundullahT-CT <cterzioglu@cybertekschool.com>
2022-09-27 22:24:33 +0200

    fixes validation

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -24,6 +24,9 @@ public class Employee {
     @NotBlank
     @Size(max = 12, min = 2)
     private String firstName;
+
+//    @NotBlank
+//    @Size(max = 12, min = 2)
     private String lastName;
 
     // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy  -   yyyy-dd-mm
